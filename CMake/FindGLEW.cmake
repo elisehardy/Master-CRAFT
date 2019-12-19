f173822e9d9a28c9b346046560879568bdd28e2d
Quentin Coumes <qcoumes@etud.u-pem.fr>
2019-12-19 23:25:29 +0100

    Update cmake

--- CMake/FindGLEW.cmake
+++ CMake/FindGLEW.cmake
@@ -1,14 +1,14 @@
 FIND_PATH(GLEW_INCLUDE_PATH GL/glew.h
-        /usr/include
-        /usr/local/include
-        /sw/include
-        /opt/local/include)
+    /usr/include
+    /usr/local/include
+    /sw/include
+    /opt/local/include)
 FIND_LIBRARY(GLEW_LIBRARY
-        NAMES GLEW glew
-        PATHS
-        /usr/lib64
-        /usr/lib
-        /usr/local/lib64
-        /usr/local/lib
-        /sw/lib
-        /opt/local/lib)
+    NAMES GLEW glew
+    PATHS
+    /usr/lib64
+    /usr/lib
+    /usr/local/lib64
+    /usr/local/lib
+    /sw/lib
+    /opt/local/lib)
